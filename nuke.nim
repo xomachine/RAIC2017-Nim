6eafc3131186ad2bd5dc89dd428292fb65e08a91
xomachine <xomachiner@gmail.com>
2017-12-07 03:02:37 +0300

    Trying to fix nukes again

--- nuke.nim
+++ nuke.nim
@@ -10,7 +10,7 @@ from enhanced import EVehicle
 from gparams import flyers
 from vehicles import resolve, toGroup
 from utils import Point, getSqDistance, debug
-from fastset import `-`
+from fastset import `-`, card
 from formation_info import FormationInfo
 from model.vehicle_type import VehicleType
 from model.move import Move
@@ -24,41 +24,42 @@ proc initNuke(): Behavior =
   let do_reset = proc() =
     target = (x: -1'f64, y: -1'f64)
     stopped = false
+  do_reset()
   result.tick = proc(ws: WorldState, fi: FormationInfo): BehaviorStatus =
     let me = ws.players[Players.me]
     if me.next_nuclear_strike_tick_index > 0:
       return BehaviorStatus.hold
     elif me.remainingNuclearStrikeCooldownTicks > 0:
+      do_reset()
       return BehaviorStatus.inactive
     let v = ws.vehicles
     let hcenter = fi.center
     let sqVision = ws.game.fighterVisionRange * ws.game.fighterVisionRange
-    for cluster in v.byEnemyCluster:
+    #let vplusc = (distanceToCenter: 1.0, point: hcenter) & @(fi.vertices)
+    debug("Iterating over " & $v.byEnemyCluster.len & " clusters...")
+    for i, cluster in v.byEnemyCluster.pairs():
       let center = cluster.center
-      for vv in fi.vertices:
-        if vv.distanceToCenter == 0:
-          continue
-        let distance = vv.point.getSqDistance(center)
-        if distance < sqVision * 0.9:
-          target = center
-          if stopped:
-            return BehaviorStatus.actUnselected
-          else:
-            return BehaviorStatus.act
+     # for vv in vplusc:
+     #   if vv.distanceToCenter == 0:
+     #     continue
+     #   let distance = vv.point.getSqDistance(center)
       let distance = hcenter.getSqDistance(center)
+      debug("Cluster " & $i & " has " & $(cluster.cluster.card) &
+            " units and sits " & $distance & " away")
       if distance < sqVision * 0.9:
         target = center
         if stopped:
           return BehaviorStatus.actUnselected
         else:
           return BehaviorStatus.act
+    do_reset()
     return BehaviorStatus.inactive
   result.action = proc (ws: WorldState, fi: FormationInfo, m: var Move) =
     if not stopped:
       debug("Stopping...")
       m.action = ActionType.MOVE
-      m.x = 0.0
-      m.y = 0.0
+      m.x = 0.01
+      m.y = 0.01
       stopped = true
     elif target.x >= 0 and target.y >= 0:
       for u in fi.units:
