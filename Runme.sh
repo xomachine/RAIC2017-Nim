40ac2d5811a1144e119580bc47397d37b6ee661e
xomachine <xomachiner@gmail.com>
2017-12-14 00:43:03 +0300

    Updated runscript

--- Runme.sh
+++ Runme.sh
@@ -6,8 +6,8 @@ cd ../local-runner-ru
 sh local-runner.sh
 cd $curdir
 sleep 3
-#nim c -d:stdebug runner.nim
-nim c -d:drawPFGrid runner.nim
+#nim c -d:stdebug=fieldbehavior.nim,pbinitial.nim,enemyfield.nim,nukealert.nim runner.nim
+nim c -d:stdebug=enemyfield.nim,pbnuke.nim -d:drawGrid=4 runner.nim
 #nim c runner.nim
 #nim c -d:release runner.nim
 #nim c --debugger:native runner.nim
@@ -15,5 +15,6 @@ nim c -d:drawPFGrid runner.nim
 bash -c 'sleep 2; ./runner "127.0.0.1" "31002" "0000000000000000" &> second.log' &
 #gdb ./runner
 #./runner
+#./runner 2> /first.log
 #valgrind --tool=callgrind ./runner
-./runner | sed -n 's/^!//p' | python plotter.py
+./runner 2>first.log | sed -n 's/^!//p' | python plotter.py
